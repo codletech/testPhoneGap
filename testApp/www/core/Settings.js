8473f96c776e5805d184131061db9830753ae4b5
codletech <codletech@gmail.com>
2014-10-25 21:02:27 +0300

    uploading settings.js

--- testApp/www/core/Settings.js
+++ testApp/www/core/Settings.js
@@ -1,18 +1,1 @@
-/**
- * Created by dvircn on 14/10/14.
- */
-var CSettings = {
-    settings: {},
-    set: function(key,value){
-        CSettings.settings[key] = value;
-    },
-    get: function(key){
-        return CSettings.settings[key];
-    },
-    initialize: function(){
-        CSettings.set('appID','54454949ff3211020039f812');
-    }
-}
-
-CSettings.initialize();
-
+var CSettings = {      settings: {},      set: function(key,value){          CSettings.settings[key] = value;      },      get: function(key){          return CSettings.settings[key];      },      initialize: function(){          CSettings.set('appID','54495b5b9992aa0200e711b9');      }  }  CSettings.initialize();
