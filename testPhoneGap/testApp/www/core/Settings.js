5f64fd642c7a8d4a87a2f987f8a8482dcda4550b
codletech <codletech@gmail.com>
2014-10-27 23:38:25 +0200

    Delete Settings.js

--- testPhoneGap/testApp/www/core/Settings.js
+++
@@ -1,1 +0,0 @@
-var CSettings = {      settings: {},      set: function(key,value){          CSettings.settings[key] = value;      },      get: function(key){          return CSettings.settings[key];      },      initialize: function(){          CSettings.set('appID','54495b5b9992aa0200e711b9');      }  }  CSettings.initialize();
